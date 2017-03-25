b0ca9f518b7980d627fbd55c8475292988b8dc19
Mate Zec <mate.zec@gmail.com>
2017-03-25 21:46:00 +0100

    cleanup

--- gunshot/gunshot.ino
+++ gunshot/gunshot.ino
@@ -1,61 +1,25 @@
 #include <SD.h>
 
 /******************************************************************************
-   sound_detector_demo.ino
-   Sound detector sample sketch
-   Byron Jacquot @ SparkFun Electronics
-   February 19, 2014
-   https://github.com/sparkfun/Sound_Detector
-
-   This sketch demonstrates the use of the Sparkfun Sound Detector board.
-
-   The Sound Detector is a small board that combines a microphone and some
-   processing circuitry.  It provides not only an audio output, but also a
-   binary indication of the presence of sound and an analog representation
-   of it's amplitude.
-
-   This sketch demonstrates two different modes of usage for the Sound
-   Detector.  The gate output (a binary indication that is high when sound
-   is present, and low when conditions are quiet) is used to fire a pin-change
-   ISR, which lights an LED when the sound is present.  The envelope output
-   (an analog voltage to rises to indicate the amplitude of the sound) is
-   sampled in the loop(), and it prints an indication of the level to the
-   serial terminal.
-
-   For more details about the Sound Detector, please check the hookup guide.
-
-   Connections:
-   The Sound Detector is connected to the Adrduino as follows:
-   (Sound Detector -> Arduino pin)
-   GND → GND
-   VCC → 5V
-   Gate → Pin 2
-   Envelope → A0
-
-   Resources:
-   Additional library requirements: none
-
-   Development environment specifics:
-   Using Arduino IDe 1.0.5
-   Tested on Redboard, 3.3v/8MHz and 5v/16MHz ProMini hardware.
-
-   This code is beerware; if you see me (or any other SparkFun employee) at the
-   local, and you've found our code helpful, please buy us a round!
-
-   Distributed as-is; no warranty is given.
+  gunshot.ino
+  Gunshot detector
+  mzec @ BIOM
+  2017-03-23
+  code available at:
+    https://github.com/udruga-biom/gunshot
+  based on:
+    https://github.com/sparkfun/Sound_Detector
+    https://www.arduino.cc/en/tutorial/smoothing
  ******************************************************************************/
 
-// Define hardware connections
-#define PIN_GATE_IN 2
-#define IRQ_GATE_IN 0
-#define PIN_LED_OUT 4
+// define input pin
 #define PIN_ANALOG_IN A0
 
-// Change to alter smoothness of output
-const int numReadings = 10;
-const int treshold = 15;
-const int numDelay = 3;
-const int eventDuration = 500;
+// parameters, change for different output
+const int numReadings = 25; // smoothing window size
+const int treshold = 25; // sensitivity of detector
+const int numDelay = 10; // delay between readings
+const int eventDuration = 1000; // delay after sound detection
 
 const uint8_t BUFFER_SIZE = 25;
 char fileName[] = "demoFile.txt"; // SD library only supports up to 8.3 names
@@ -108,11 +72,10 @@ void loop()
   int value;
   String valueString;
 
-  // Check the envelope input
+  // read the envelope input
   value = analogRead(PIN_ANALOG_IN);
-  //value = digitalRead(PIN_GATE_IN);
 
-  // Smoother
+  // smoother
   // subtract the last reading:
   total = total - readings[readIndex];
   // read from the sensor:
--- gunshot_serial/gunshot_serial.ino
+++ gunshot_serial/gunshot_serial.ino
@@ -1,60 +1,25 @@
 #include <SD.h>
 
 /******************************************************************************
-   sound_detector_demo.ino
-   Sound detector sample sketch
-   Byron Jacquot @ SparkFun Electronics
-   February 19, 2014
-   https://github.com/sparkfun/Sound_Detector
-
-   This sketch demonstrates the use of the Sparkfun Sound Detector board.
-
-   The Sound Detector is a small board that combines a microphone and some
-   processing circuitry.  It provides not only an audio output, but also a
-   binary indication of the presence of sound and an analog representation
-   of it's amplitude.
-
-   This sketch demonstrates two different modes of usage for the Sound
-   Detector.  The gate output (a binary indication that is high when sound
-   is present, and low when conditions are quiet) is used to fire a pin-change
-   ISR, which lights an LED when the sound is present.  The envelope output
-   (an analog voltage to rises to indicate the amplitude of the sound) is
-   sampled in the loop(), and it prints an indication of the level to the
-   serial terminal.
-
-   For more details about the Sound Detector, please check the hookup guide.
-
-   Connections:
-   The Sound Detector is connected to the Adrduino as follows:
-   (Sound Detector -> Arduino pin)
-   GND → GND
-   VCC → 5V
-   Gate → Pin 2
-   Envelope → A0
-
-   Resources:
-   Additional library requirements: none
-
-   Development environment specifics:
-   Using Arduino IDe 1.0.5
-   Tested on Redboard, 3.3v/8MHz and 5v/16MHz ProMini hardware.
-
-   This code is beerware; if you see me (or any other SparkFun employee) at the
-   local, and you've found our code helpful, please buy us a round!
-
-   Distributed as-is; no warranty is given.
+  gunshot_serial.ino
+  Gunshot detector serial debugger
+  mzec @ BIOM
+  2017-03-23
+    code available at:
+    https://github.com/udruga-biom/gunshot
+  based on:
+    https://github.com/sparkfun/Sound_Detector
+    https://www.arduino.cc/en/tutorial/smoothing
  ******************************************************************************/
 
-// Define hardware connections
-#define PIN_GATE_IN 2
-#define IRQ_GATE_IN  0
+// define input pin
 #define PIN_ANALOG_IN A0
 
-// Change to alter smoothness of output
-const int numReadings = 30;
-const int treshold = 25;
-const int numDelay = 10;
-const int eventDuration = 1000;
+// parameters, change for different output
+const int numReadings = 30; // smoothing window size
+const int treshold = 25; // sensitivity of detector
+const int numDelay = 10; // delay between readings
+const int eventDuration = 1000; // delay after sound detection
 
 int readings[numReadings];
 int readIndex = 0;
@@ -80,11 +45,11 @@ void loop()
   String valueString;
   int paf = 0;
 
-  // Check the envelope input
+  // read the envelope input
   value = analogRead(PIN_ANALOG_IN);
-  //value = digitalRead(PIN_GATE_IN);
-
-  // Smoother
+  
+  // smoother
+  // subtract the last reading:
   total = total - readings[readIndex];
   // read from the sensor:
   readings[readIndex] = value;
